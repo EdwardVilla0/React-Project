6f803779ecbdd5bdd29f2c684af161feb8b7f241
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-27 13:35:56 -0700

    adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
 import HelloWorld from './Components/HelloWorld'
-import CounterExample from './Components/CounterExample'
+import Header from './Components/Header'
+
 
 function App() {
   return (
     <div>
+      <Header />
+
       <HelloWorld name="jared" />
     </div>
   );
