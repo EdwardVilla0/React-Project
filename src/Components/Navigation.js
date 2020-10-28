ada631cc441824e7bdc11593ba488cf4828d9826
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-28 12:03:21 -0700

    adding transition

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTransition, animated } from 'react-spring';
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
+    const transitions = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
 
-    let menu
+    // className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+    // className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
 
-    if (showMenu) {
-        menu =
-            <div
-                className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
-            >
-                the menu
-        </div>
-    }
+
+
+
 
     return (
         <nav>
@@ -24,8 +26,30 @@ function Navigation() {
                     onClick={() => setShowMenu(!showMenu)}
                 />
             </span>
+            {
+                transitions.map(({ item, key, props }) =>
+                    item &&
+                    <animated.div
+                        key={key}
+                        style={props}
+                        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+                        onClick={() => setShowMenu(false)}>
 
-            {menu}
+                    </animated.div>
+                )
+            }
+            {
+                transitions.map(({ item, key, props }) =>
+                    item &&
+                    <animated.div
+                        key={key}
+                        style={props}
+                        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+                        this is the menu
+
+                    </animated.div>
+                )
+            }
         </nav>
     )
 }
