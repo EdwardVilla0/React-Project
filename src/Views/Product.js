1ccf28b38bfda4c4945dac2233f7a983aee6996c
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-28 14:11:08 -0700

    added loading icon

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,36 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function Product() {
-    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/3`
-    const [product, setProduct] = useState(null)
+    const { id } = useParams()
+    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
 
     let content = null
 
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null,
+        })
         axios.get(url)
             .then(response => {
-                setProduct(response.data)
+                setProduct({
+                    loading: false,
+                    data: response.data
+                })
             })
     }, [url])
 
-    if (product) {
+    if (product.loading) {
+        content = <Loader></Loader>
+    }
+
+    if (product.data) {
         content =
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                     <img
-                        src={product.images[0].imageUrl}
-                        alt={product.name}
+                        src={product.data.images[0].imageUrl}
+                        alt={product.data.name}
                     />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
 
             </div>
