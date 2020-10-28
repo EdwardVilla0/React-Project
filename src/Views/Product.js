90317ebc5f4c5ab94af69a6aea12ae15db0b31a3
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-28 13:45:29 -0700

    using api calling to get image description and price

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Product() {
+    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/3`
+    const [product, setProduct] = useState(null)
+
+    let content = null
+
+    useEffect(() => {
+        axios.get(url)
+            .then(response => {
+                setProduct(response.data)
+            })
+    }, [url])
+
+    if (product) {
+        content =
+            <div>
+                <h1 className="text-2xl font-bold mb-3">
+                    {product.name}
+                </h1>
+                <div>
+                    <img
+                        src={product.images[0].imageUrl}
+                        alt={product.name}
+                    />
+                </div>
+                <div className="font-bold text-xl mb-3">
+                    $ {product.price}
+                </div>
+                <div>
+                    {product.description}
+                </div>
+
+            </div>
+    }
+
     return (
         <div>
-            <h1>this is the product page</h1>
+            {content}
         </div>
     )
+
 }
 
 export default Product
