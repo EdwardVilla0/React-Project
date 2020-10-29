3bb829a45ebcf759e5a91a0013df94918eabba06
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-29 14:30:32 -0700

    adding list of products on home screen

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,11 +1,67 @@
-import React from 'react'
-import HelloWorld from '../Components/HelloWorld'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Loader from '../Components/Loader'
+
 
 function Home() {
+    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products?page=1&limit=10`
+    const [products, setProducts] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
+
+    useEffect(() => {
+        setProducts({
+            loading: true,
+            data: null,
+            error: false
+        })
+        axios.get(url)
+            .then(response => {
+                setProducts({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+            })
+            .catch(error => {
+                setProducts({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
+            })
+    }, [url])
+
+    let content = null
+
+    if (products.error) {
+        content = <p>
+            There was an error. Please refresh or try again later.
+        </p>
+    }
+
+    if (products.loading) {
+        content = <Loader></Loader>
+    }
+
+    if (products.data) {
+        content =
+            products.data.map((product, key) =>
+                <div>
+                    {product.name}
+                </div>
+            )
+    }
+
+
     return (
         <div>
-            <h1 className="font-bold text-2xl">This is the home page</h1>
-            <HelloWorld />
+            <h1 className="font-bold text-2xl">
+                Best Sellers
+                </h1>
+            {content}
         </div>
     )
 }
