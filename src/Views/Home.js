5579041fee9b8bb22437ae975fcd0944e8b52eca
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-29 15:58:38 -0700

    created a hook for http request

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,39 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import Loader from '../Components/Loader'
+import axios from 'axios'
+
 import ProductCard from '../Components/ProductCard'
+import { useAxiosGet } from '../Hooks/HttpRequests'
 
 
 function Home() {
     const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products?page=1&limit=10`
-    const [products, setProducts] = useState({
-        loading: false,
-        data: null,
-        error: false
-    })
 
-    useEffect(() => {
-        setProducts({
-            loading: true,
-            data: null,
-            error: false
-        })
-        axios.get(url)
-            .then(response => {
-                setProducts({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch(error => {
-                setProducts({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            })
-    }, [url])
+    let products = useAxiosGet(url)
 
     let content = null
 
